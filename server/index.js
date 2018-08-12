ace1fd1c1a40c4a5fc66a5a755a1229dae393c7b
mjcampagna <mjcampagna@gmail.com>
2018-08-11 22:23:11 -0700

    Serve html, css, js files with the correct Content-Type

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 const url = require('url');
 const fs = require('fs');
 
@@ -16,12 +17,26 @@ const server = http.createServer( (req, res) => {
 				filePath += '/index.html';
 			}
 
-			fs.readFile( filePath, (err, data ) => {
+	fs.readFile( filePath, (err, data ) => {
 				if ( err ) {
 					res.statusCode = 500;
 					res.end( `Server error: "${err}"` );
 				} else {
-					res.end( data.toString('utf-8') );
+					const ext = path.extname( req.url );
+					switch(ext) {
+
+						case '.css':
+							res.writeHead( 200, {'Content-Type': 'text/css'} );
+							res.end( data.toString('utf-8') );
+	
+						case '.js':
+							res.writeHead( 200, {'Content-Type': 'text/javascript'} );
+							res.end( data.toString('utf-8') );
+	
+						default:
+							res.end( data.toString('utf-8') );
+
+					}
 				}
 			});
 		}
