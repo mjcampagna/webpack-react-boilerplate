bde0bec473a5ad025bbc14fe9e9dd21ea6223e2e
mjcampagna <mjcampagna@gmail.com>
2018-08-12 19:56:36 -0700

    Implements an Icon component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,9 @@
 import React, { Component, Fragment } from 'react';
 
+import Icon from '../../utilities/Icon.js';
 import Portal from '../Portal';
 
 export default class Modal extends Component {
-	componentDidMount() {
-	}
-
 	render() {
 		const { children, on, toggle } = this.props;
 		return (
@@ -14,7 +12,9 @@ export default class Modal extends Component {
 					<div id="modal-overlay">
 						<div id="modal-background" onClick={toggle}></div>
 						<div id="modal-card">
-							<button id="modal-button-close" onClick={toggle}>Close</button>
+							<button id="modal-button-close" onClick={toggle}>
+								<Icon name="close" />
+							</button>
 							{children}
 						</div>
 					</div>
